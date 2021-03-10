8b67306e5388242445b1a1aeee9f303c4b7eb7b3
An <anchka.gor@yandex.ru>
2021-03-10 21:58:23 +0300

    Filter onClick is done

--- src/components/homework-page/Filters/filter/DrawFilter.tsx
+++ src/components/homework-page/Filters/filter/DrawFilter.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FilterParameter } from '../../../../classes/FilterParameter';
 import './Filter.css';
 
@@ -5,17 +6,26 @@ interface FilterToRender {
     filterToRender: FilterParameter;
 }
 
+
+
 function DrawFilter(attributes: FilterToRender) {
+    const [listVisibility, setListVisibility] = useState("hidden");
+
+    const showCheckBoxes = () => {
+        if (listVisibility=="hidden"){
+        setListVisibility("");}
+        else setListVisibility("hidden");
+    }
     return (
         <div className="col">
             <div className="multiselect">
-                <div className="selectBox">
-                    <select>
+                <div className="selectBox" onClick={showCheckBoxes}>
+                    <select >
                         <option>{attributes.filterToRender.FilterType}</option>
                     </select>
                     <div className="overSelect"></div>
                 </div>
-                <div className="listItems">{
+                <div className={"listItems " + (listVisibility)}>{
                     attributes.filterToRender.Content.map(item => (
                         <label><input type="checkbox" />{item}</label>
                     ))
