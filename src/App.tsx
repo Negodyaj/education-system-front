a511ac4d9ea8b0cbccee5ec6072764188a7d6b24
Antoshka <ant.efremenkov@yandex.ru>
2021-03-10 12:11:57 +0300

    login behavior for App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,54 @@ import './App.css';
 import CustomList from './components/custom-list/CustomList';
 import Cards from './Cards';
 import { Link, Switch, Route } from 'react-router-dom';
+import LoginForm from './components/login-form/LoginForm';
 
 function App() {
-    
+    const isLoggedIn: boolean = true;
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <nav>
-                    <Link to="/user-cards">User cards</Link>
-                    <Link to="/custom-list">Custom list</Link>
-                </nav>
+            <header>
+                <div className="logo-container">
+                    <img src={logo} className="app-logo" alt="logo" />
+                </div>
+                <div className="header-user-actions">
+                    {
+                        isLoggedIn && <button>Log out</button>
+                    }
+                </div>
             </header>
-            <main>
-                <Switch>
-                    <Route exact path="/">
-                        <h1>Choose your destiny</h1>
-                    </Route>
-                    <Route path="/user-cards">
-                        <Cards />
-                    </Route>
-                    <Route path="/custom-list">
-                        <CustomList />
-                    </Route>
-                </Switch>
-            </main>
+            <div className="main-content">
+                <aside>
+                    {
+                        isLoggedIn ?
+                            <nav>
+                                <Link to="/user-cards">User cards</Link>
+                                <Link to="/custom-list">Custom list</Link>
+                            </nav>
+                            :
+                            <h2>Залогиньтесь!</h2>
+                    }
+                </aside>
+                <main>
+                    {
+                        isLoggedIn ? 
+                        <Switch>
+                            <Route exact path="/">
+                                <h1>Choose your destiny</h1>
+                            </Route>
+                            <Route path="/user-cards">
+                                <Cards />
+                            </Route>
+                            <Route path="/custom-list">
+                                <CustomList />
+                            </Route>
+                        </Switch>
+                        :
+                        <LoginForm />
+                    }
+                </main>
+            </div>
         </div>
     );
 }
---
+++ src/components/login-form/LoginForm.tsx
@@ -0,0 +1,12 @@
+import './LoginForm.css';
+
+function LoginForm() {
+    return (
+        <div className="login-form">
+            <input type="text"/>
+            <input type="text"/>
+        </div>
+    )
+}
+
+export default LoginForm;
