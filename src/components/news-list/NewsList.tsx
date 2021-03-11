1ba11c6d73e91ce1a74c9ac85761c56000beb16e
Vadim <clandestineblast@gmail.com>
2021-03-12 01:58:38 +0300

    Add news alerts for student

--- src/components/news-list/NewsList.tsx
+++ src/components/news-list/NewsList.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./NewsList.css";
+import './components/news-alert/NewsAlert'
+import NewsAlert from "./components/news-alert/NewsAlert";
 
+const news = [
+    'Преподаватель принял вашу домашнюю работу "Циклы".',
+    'Защита курса переносится на 24 сентября в 12.00!',
+    'У вас осталось 2 дня на выполнение домашней работы "Двумерные массивы"!',
+    'Необходимо произвести оплату курса 15 сентября!'
+]
 
 function NewsList() {
     return (
-        <ul>
-            <h2> News</h2>
-        </ul>
+        <div className="alerts-window">
+            {
+                news.map(text => <NewsAlert text={text}/>)
+            }
+        </div>
     )
 }
 
