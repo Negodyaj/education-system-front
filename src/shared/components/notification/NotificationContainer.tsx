cd1b29aad86391417e66cd4f8141d54a7c855fc0
Vadim <clandestineblast@gmail.com>
2021-03-21 20:56:07 +0300

    FU47 Random new notification buttons

--- src/shared/components/notification/NotificationContainer.tsx
+++ src/shared/components/notification/NotificationContainer.tsx
@@ -1,49 +1,43 @@
+import { useState } from 'react';
+import NotificationData from '../../interfaces/NotificationData';
+import { generateTestNotification } from './generateTestNotification';
 import Notification from './Notification';
 import './NotificationContainer.css';
 
 function NotificationContainer() {
 
-    const dismissibleNotifications = [
-        {
-            type: "information",
-            text: 'Защита курса переносится на 24 сентября в 12.00!',
-            isDismissible: true
-        },
-        {
-            type: "success",
-            text: 'Преподаватель принял вашу домашнюю работу "Циклы".',
-            isDismissible: true
-        },
-        {
-            type: "error",
-            text: 'Ошибка',
-            isDismissible: true
-        },        
-    ]
+    const [dismissibleNotifications, setDismissibleNotifications] = useState<NotificationData[]>([]);
+    const [nonDismissibleNotifications, setNonDismissibleNotifications] = useState<NotificationData[]>([]);
 
-    const nonDismissibleNotifications = [
-        {
-            type: "information",
-            text: 'Необходимо произвести оплату курса 15 сентября!',
-            isDismissible: false
-        },
-    ]
+    const generateNotification = (isDismissible: boolean) => {
+        const newNotification = generateTestNotification(isDismissible);
+        const newState = isDismissible ? 
+            [newNotification, ...dismissibleNotifications] :
+            [newNotification, ...nonDismissibleNotifications]
+        isDismissible ? 
+            setDismissibleNotifications(newState) : 
+            setNonDismissibleNotifications(newState);
+    }
     
     return (
         <div className="notification-container">
             <div className="non-dismissible-notifications">
                 {nonDismissibleNotifications.map(notification => {
                     return (
-                        <Notification {...notification}/>
+                        <Notification key={notification.text} {...notification}/>
                     )
                 })}
             </div>
             <div className="dismissible-notifications">
             {dismissibleNotifications.map(notification => {
                     return (
-                        <Notification {...notification}/>
+                        <Notification key={notification.text} {...notification}/>
                     )
                 })}
+            </div>
+            <div className="test-button-container">
+                <button onClick={() => generateNotification(true)}>dismissibleNotification</button>
+                <button onClick={() => generateNotification(false)}>nonDismissibleNotification</button>
             </div>
         </div>
     )
