aae125d613146b9c3e9d87437dc04c8354bc8549
Nick <nikita_romanov_92@bk.ru>
2021-03-23 20:13:29 +0300

    FU-32 Add courseDelete, modalWindow

--- src/components/courses-page/CoursesPage.tsx
+++ src/components/courses-page/CoursesPage.tsx
@@ -1,17 +1,40 @@
+import React, { useState } from 'react';
 import './CoursesPage.css';
+import ModalWindowDelete from './modal-window/ModalWindowDelete';
+
 
 interface CoursesPageProps {
     roleId: number;
 }
+let idCourseDelete = 0;
+let idCoursesDelete: number[] = [];
+
 
 function CoursesPage(props: CoursesPageProps) {
-
+    
     const courses = [ 
         { id: 1, name: 'C# base' }, 
         { id: 2, name: 'Backend' }, 
         { id: 3, name: 'Frontend' },
         { id: 4, name: 'Mobile development' } 
     ];
+
+    const [isModalDelete, setIsModalDelete] = useState(false);
+    const [coursesList, setCoursesList] = useState(courses);
+
+    const openModal = (id: number) => {
+        setIsModalDelete(true);
+        idCourseDelete = id;
+    }
+
+    const onDeleteHandler = (num: number) => {
+        setIsModalDelete(false);
+        if (num === 1) {
+            idCoursesDelete.push(idCourseDelete);
+            setCoursesList(courses.filter((item) => !(idCoursesDelete.includes(item.id))));
+        }
+    }
+
 
     return(
         <div className="course-container">
@@ -20,17 +43,18 @@ function CoursesPage(props: CoursesPageProps) {
             </div>
             <div className="courses-list">
                 {
-                    courses.map(item => (
+                    coursesList.map(item => (
                         <div key={item.id} className="course">
                             <div className="course-name">{item.name}</div>
                             <div className="course-update-delete">
                                 <button className='button-update'></button>
-                                <button className='button-delete'></button>
+                                <button onClick={() => openModal(item.id)} className='button-delete'></button>
                             </div>
                         </div>
                     ))
                 }
             </div>
+            { isModalDelete && <ModalWindowDelete onClickDelete={onDeleteHandler}/>}
         </div>
     )
 }
