6f88c0fa8a0b88c57a216d6a672fc7d7b71c769d
Dmitriy Filimonov <banodannon@gmail.com>
2021-03-31 11:33:47 +0300

    FU58-getDate instead getDay in datepicker

--- src/shared/components/date-picker/DatePickerComponent.tsx
+++ src/shared/components/date-picker/DatePickerComponent.tsx
@@ -11,8 +11,9 @@ function DatePickerComponent(props: DatePickerComponentProps) {
   let date: number[] = [];
   let dateConstructorArg = props.date?.concat().split('.');
   date.push(Number.parseInt(dateConstructorArg ? dateConstructorArg[2] : new Date().getFullYear().toString()));
-  date.push(Number.parseInt(dateConstructorArg ? dateConstructorArg[1] : new Date().getMonth().toString()) - 1);
-  date.push(Number.parseInt(dateConstructorArg ? dateConstructorArg[0] : new Date().getDay().toString()));
+  date.push(Number.parseInt(dateConstructorArg ? dateConstructorArg[1] : new Date().getMonth().toString()));
+  date.push(Number.parseInt(dateConstructorArg ? dateConstructorArg[0] : new Date().getDate().toString()));
+
   const [startDate, setStartDate] = useState(new Date(date[0], date[1], date[2]));
   const handleDateChange = (date: Date | [Date, Date] | null) => {
     if (date instanceof Date) {
