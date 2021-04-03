ef3d07950c0909ba5d404ba10de03369eb887797
Nick <nikita_romanov_92@bk.ru>
2021-04-03 22:41:54 +0300

    fix search icon in fontawesome

--- src/shared/fontawesome/FontawesomeIcons.tsx
+++ src/shared/fontawesome/FontawesomeIcons.tsx
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimes, faEdit, faTrash, faPlus, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faEdit, faTrash, faPlus, faSpinner, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 
-library.add(faTimes, faEdit, faTrash, faPlus, faSpinner);
+library.add(faTimes, faEdit, faTrash, faPlus, faSpinner, faSearch);
