acffa505b54cca13fbbe97e4f2cb8c9711d8f924
Anton Efremenkov <anton@clearaccept.com>
2021-04-09 14:47:20 +0300

    FU-23 new styles for datepicker

--- src/shared/components/date-picker/DatePickerComponent.tsx
+++ src/shared/components/date-picker/DatePickerComponent.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import './DatePickerComponent.css';
 
 interface DatePickerComponentProps {
   date: string | undefined,
