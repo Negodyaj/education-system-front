db6a2b1c4346216d4f2ef09e0f0bbecad9d36289
Nick <nikita_romanov_92@bk.ru>
2021-05-05 10:20:35 +0300

    FU-104 uncomment course edition

--- src/components/courses-page/course-edition/CourseEdition.tsx
+++ src/components/courses-page/course-edition/CourseEdition.tsx
@@ -26,151 +26,151 @@ interface ParamTypes {
 
 function CourseEdition() {
 
-    // const dispatch = useDispatch();
-    // const pageState = useSelector(((state: IRootState) => state.courseEditionPage));
-    // let { id } = useParams<ParamTypes>();
+    const dispatch = useDispatch();
+    const pageState = useSelector(((state: IRootState) => state.courseEditionPage));
+    let { id } = useParams<ParamTypes>();
     
-    // let themesInCourse: number[] = [];
-    // const idCourse = +id;
+    let themesInCourse: number[] = [];
+    const idCourse = +id;
 
-    // useEffect(() => {
-    //     dispatch(getThemes());
-    //     dispatch(getCourseById(idCourse));
-    // }, []);
+    useEffect(() => {
+        dispatch(getThemes());
+        dispatch(getCourseById(idCourse));
+    }, []);
 
-    // useEffect(() => {
-    //     checkThemes();
-    // }, [pageState.course.themes]);
+    useEffect(() => {
+        checkThemes();
+    }, [pageState.course.themes]);
 
-    // const [searchWord, setSearchWord] = useState('');
+    const [searchWord, setSearchWord] = useState('');
 
-    // const addNewThemeInProgramCourse = (theme: Themes) => {
-    //     if (checkTheThemeInTheCourse(theme.id)) { 
-    //         const courseTheme: CourseTheme = {idCourse: idCourse, idTheme: theme.id};
-    //         dispatch(addThemeInCourse(courseTheme));
-    //         !pageState.isDisplayingButtonOpenProgramCourse && openProgramCourse();
-    //     }
-    // }
+    const addNewThemeInProgramCourse = (theme: Themes) => {
+        if (checkTheThemeInTheCourse(theme.id)) { 
+            const courseTheme: CourseTheme = {idCourse: idCourse, idTheme: theme.id};
+            dispatch(addThemeInCourse(courseTheme));
+            !pageState.isDisplayingButtonOpenProgramCourse && openProgramCourse();
+        }
+    }
 
-    // const checkTheThemeInTheCourse = (themeId: number): boolean => {
-    //     const theme = pageState.course.themes.find(t => t.id === themeId);
-    //     return theme === undefined;
-    // } 
+    const checkTheThemeInTheCourse = (themeId: number): boolean => {
+        const theme = pageState.course.themes.find(t => t.id === themeId);
+        return theme === undefined;
+    } 
 
-    // const checkThemes = () => {
-    //     pageState.course.themes?.map((theme) => (
-    //         themesInCourse.push(theme.id)
-    //     ))
-    //     dispatch(setAllThemesInCourse(themesInCourse));
-    // }
+    const checkThemes = () => {
+        pageState.course.themes?.map((theme) => (
+            themesInCourse.push(theme.id)
+        ))
+        dispatch(setAllThemesInCourse(themesInCourse));
+    }
 
-    // const deleteThemeFromCourse = (theme: Themes) => {
-    //     const courseTheme: CourseTheme = {idCourse: idCourse, idTheme: theme.id};
-    //     dispatch(deleteThemeCourse(courseTheme));
-    // } 
+    const deleteThemeFromCourse = (theme: Themes) => {
+        const courseTheme: CourseTheme = {idCourse: idCourse, idTheme: theme.id};
+        dispatch(deleteThemeCourse(courseTheme));
+    } 
 
-    // const deleteMaterialFromCourse = (material: Material) => {
-    //     const courseMaterial: CourseMaterial = {idCourse: idCourse, idMaterial: material.id};
-    //     dispatch(deleteMaterialCourse(courseMaterial));
-    // } 
+    const deleteMaterialFromCourse = (material: Material) => {
+        const courseMaterial: CourseMaterial = {idCourse: idCourse, idMaterial: material.id};
+        dispatch(deleteMaterialCourse(courseMaterial));
+    } 
 
-    // const searchInThemes = (str: string) => {
-    //     setSearchWord(str);
-    // }
+    const searchInThemes = (str: string) => {
+        setSearchWord(str);
+    }
 
-    // const openProgramCourse = () => {
-    //     dispatch(setChangeDisplayingButtonOpenProgramCourse());
-    // }
+    const openProgramCourse = () => {
+        dispatch(setChangeDisplayingButtonOpenProgramCourse());
+    }
 
-    // const openMaterialsCourse = () => {
-    //     dispatch(setChangeDisplayingButtonOpenMaterialsCourse());
-    // }
+    const openMaterialsCourse = () => {
+        dispatch(setChangeDisplayingButtonOpenMaterialsCourse());
+    }
 
-    // return (
-    // <div className="course-edition-container">
-    //     <h3 className="current-course-header-name">{ 'Курс ' + pageState.course.name }</h3>
-    //     <div className='course-update'>
-    //         <div className='new-themes-course'>
-    //             <div className="new-themes-course-header">
-    //                 <div className="new-themes-header-text">Темы для курса</div>
-    //                 <button className="new-themes-header-button-add">
-    //                     <FontAwesomeIcon icon="plus" />
-    //                 </button>
-    //             </div>
-    //             <div className="new-themes-container">
-    //                 <SearchComponent funcSearch={searchInThemes}/>
-    //                 {
-    //                     pageState.themes?.filter(item => item.name.toLowerCase().includes(searchWord.toLowerCase()))
-    //                     .map((item) => (
-    //                         <div key={item.id} className="new-theme ">
-    //                             <div className="new-theme-name">{item.name}</div>
-    //                             <div className="new-theme-add">
-    //                                 <button onClick={() => addNewThemeInProgramCourse(item)} className="button-add-theme">
-    //                                 {
-    //                                     pageState.idThemesCourse.includes(item.id) ? <FontAwesomeIcon icon="check" /> : <FontAwesomeIcon icon="plus" />
-    //                                 }
-    //                                 </button>
-    //                             </div>
-    //                         </div>
-    //                     ))
-    //                 }
-    //             </div>
-    //         </div>
-    //         <div className="current-course-container">
-    //             <div className="program-current-course-container">
-    //                     <div className="program-course-header">
-    //                         <button onClick={openProgramCourse} className="program-course-header-button-open">
-    //                             { 
-    //                                 pageState.isDisplayingButtonOpenProgramCourse ? <FontAwesomeIcon icon="angle-down" /> : <FontAwesomeIcon icon="angle-up" /> 
-    //                             }
-    //                         </button>
-    //                         <div className="program-course-header-text">Программа курса</div>
-    //                     </div>
-    //                 <div className="program-course">
-    //                     { pageState.isDisplayingButtonOpenProgramCourse &&
-    //                         pageState.course.themes?.map((theme) => (
-    //                             <div key={theme.id} className="theme">
-    //                                 <div className="theme-name">{theme.name}</div>
-    //                                 <div className="theme-delete">
-    //                                     <button onClick={() => deleteThemeFromCourse(theme)} className='button-theme-delete'>
-    //                                         <FontAwesomeIcon icon="minus" />
-    //                                     </button>
-    //                                 </div>
-    //                             </div>
-    //                         ))
-    //                     }
-    //                 </div>
-    //             </div>
-    //             <div className="materials-current-course-container">
-    //                 <div className={"materials-course-header"}>
-    //                     <button onClick={openMaterialsCourse} className="materials-course-header-button-open">
-    //                         { 
-    //                             pageState.isDisplayingButtonOpenMaterialsCourse ? <FontAwesomeIcon icon="angle-down" /> : <FontAwesomeIcon icon="angle-up" /> 
-    //                         }
-    //                     </button>
-    //                     <div className="materials-course-header-text">Материалы курса</div>
-    //                 </div>
-    //                 <div className="materials-course">
-    //                     { pageState.isDisplayingButtonOpenMaterialsCourse &&
-    //                         pageState.course.materials?.map((material) => (
-    //                             <div key={material.id} className="material">
-    //                                 <div className="material-content">
-    //                                     <a href={material.link} title={material.link} target="_blank" className="link-material">{material.description}</ a>
-    //                                 </div>
-    //                                 <div className="material-delete">
-    //                                     <button onClick={() => deleteMaterialFromCourse(material)} className='button-material-delete'>
-    //                                         <FontAwesomeIcon icon="minus" />
-    //                                     </button>
-    //                                 </div>
-    //                             </div>
-    //                         ))
-    //                     }
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
-    // )
+    return (
+    <div className="course-edition-container">
+        <h3 className="current-course-header-name">{ 'Курс ' + pageState.course.name }</h3>
+        <div className='course-update'>
+            <div className='new-themes-course'>
+                <div className="new-themes-course-header">
+                    <div className="new-themes-header-text">Темы для курса</div>
+                    <button className="new-themes-header-button-add">
+                        <FontAwesomeIcon icon="plus" />
+                    </button>
+                </div>
+                <div className="new-themes-container">
+                    <SearchComponent funcSearch={searchInThemes}/>
+                    {
+                        pageState.themes?.filter(item => item.name.toLowerCase().includes(searchWord.toLowerCase()))
+                        .map((item) => (
+                            <div key={item.id} className="new-theme ">
+                                <div className="new-theme-name">{item.name}</div>
+                                <div className="new-theme-add">
+                                    <button onClick={() => addNewThemeInProgramCourse(item)} className="button-add-theme">
+                                    {
+                                        pageState.idThemesCourse.includes(item.id) ? <FontAwesomeIcon icon="check" /> : <FontAwesomeIcon icon="plus" />
+                                    }
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="current-course-container">
+                <div className="program-current-course-container">
+                        <div className="program-course-header">
+                            <button onClick={openProgramCourse} className="program-course-header-button-open">
+                                { 
+                                    pageState.isDisplayingButtonOpenProgramCourse ? <FontAwesomeIcon icon="angle-down" /> : <FontAwesomeIcon icon="angle-up" /> 
+                                }
+                            </button>
+                            <div className="program-course-header-text">Программа курса</div>
+                        </div>
+                    <div className="program-course">
+                        { pageState.isDisplayingButtonOpenProgramCourse &&
+                            pageState.course.themes?.map((theme) => (
+                                <div key={theme.id} className="theme">
+                                    <div className="theme-name">{theme.name}</div>
+                                    <div className="theme-delete">
+                                        <button onClick={() => deleteThemeFromCourse(theme)} className='button-theme-delete'>
+                                            <FontAwesomeIcon icon="minus" />
+                                        </button>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="materials-current-course-container">
+                    <div className={"materials-course-header"}>
+                        <button onClick={openMaterialsCourse} className="materials-course-header-button-open">
+                            { 
+                                pageState.isDisplayingButtonOpenMaterialsCourse ? <FontAwesomeIcon icon="angle-down" /> : <FontAwesomeIcon icon="angle-up" /> 
+                            }
+                        </button>
+                        <div className="materials-course-header-text">Материалы курса</div>
+                    </div>
+                    <div className="materials-course">
+                        { pageState.isDisplayingButtonOpenMaterialsCourse &&
+                            pageState.course.materials?.map((material) => (
+                                <div key={material.id} className="material">
+                                    <div className="material-content">
+                                        <a href={material.link} title={material.link} target="_blank" className="link-material">{material.description}</ a>
+                                    </div>
+                                    <div className="material-delete">
+                                        <button onClick={() => deleteMaterialFromCourse(material)} className='button-material-delete'>
+                                            <FontAwesomeIcon icon="minus" />
+                                        </button>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default CourseEdition;
