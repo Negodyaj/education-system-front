56dc6399ec8274cede89473143a4a2a830b34555
Nick <nikita_romanov_92@bk.ru>
2021-05-09 21:32:09 +0300

    FU-122 prettier fix

--- src/components/group-page/attendance/Attendance.tsx
+++ src/components/group-page/attendance/Attendance.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { isTypeNode } from "typescript";
-import { IRootState } from "../../../store";
-import { getGroupToViewById } from "../../../store/group-info-component/thunk";
-import { getAttendanceByLessonId } from "../../../store/group-page/attendance/thunk";
-import { getLessonsByGroup } from "../../../store/group-page/lesson/thunk";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { isTypeNode } from 'typescript';
+
+import { IRootState } from '../../../store';
+import { getGroupToViewById } from '../../../store/group-info-component/thunk';
+import { getAttendanceByLessonId } from '../../../store/group-page/attendance/thunk';
+import { getLessonsByGroup } from '../../../store/group-page/lesson/thunk';
+
 import {
   AttendanceHead,
   AttendanceList,
-  AttendaneVisible,
+  AttendanceVisible,
   BlockMenu,
   ColumnHead,
   ColumnInfo,
   RowAttandance,
   TableAttendance,
-  TableHead
-} from "./AttendanceStyled";
+} from './AttendanceStyled';
 
 const Attandance = () => {
   const dispatch = useDispatch();
@@ -33,42 +34,43 @@ const Attandance = () => {
     dispatch(getGroupToViewById(14));
     dispatch(getLessonsByGroup());
     // dispatch(getAttendanceByLessons(attendance.lessonList))
-    lessonDataForColumnName.map((item) => {
+    lessonDataForColumnName.forEach((item) => {
       dispatch(getAttendanceByLessonId(item.id));
+    });
+  }, []);
 
-    return(
-        <AttendanceList>
-          <AttendanceHead>
-            <BlockMenu> Тут лежит какое-то меню для сортировок фильтров и тп</BlockMenu>
-          </AttendanceHead>
-          <AttendaneVisible>
-          <TableAttendance>
-              <RowAttandance>
-                <ColumnHead>id</ColumnHead>
-                <ColumnHead>ФИО</ColumnHead>
-                <ColumnHead>%</ColumnHead>
-                {
-                  lessonDataForColumnName.map(item => (
-                    <ColumnHead>
-                      {item.lessonDate}
-                      {/* {dispatch(getAttendanceByLessonId(item.id))} */}
-                    </ColumnHead>
-                  ))
-                }
-              </RowAttandance>
-            {
-              studentGroup.map(item => (
-                <RowAttandance>
-                  <ColumnInfo>{ item.id}</ColumnInfo>
-                  <ColumnInfo>{`${item.firstName} ${item.lastName}`}</ColumnInfo>
-                  <ColumnInfo>{ lessonDataForColumnName.length }</ColumnInfo>
-                </RowAttandance>
-              ))
-            }
-              </TableAttendance>
-          </AttendaneVisible>
-        </AttendanceList>
-    )
-}
+  return (
+    <AttendanceList>
+      <AttendanceHead>
+        <BlockMenu>
+          {' '}
+          Тут лежит какое-то меню для сортировок фильтров и тп
+        </BlockMenu>
+      </AttendanceHead>
+      <AttendanceVisible>
+        <TableAttendance>
+          <RowAttandance>
+            <ColumnHead>id</ColumnHead>
+            <ColumnHead>ФИО</ColumnHead>
+            <ColumnHead>%</ColumnHead>
+            {lessonDataForColumnName.map((item) => (
+              <ColumnHead>
+                {item.lessonDate}
+                {/* {dispatch(getAttendanceByLessonId(item.id))} */}
+              </ColumnHead>
+            ))}
+          </RowAttandance>
+          {studentGroup.map((item) => (
+            <RowAttandance>
+              <ColumnInfo>{item.id}</ColumnInfo>
+              <ColumnInfo>{`${item.firstName} ${item.lastName}`}</ColumnInfo>
+              <ColumnInfo>{lessonDataForColumnName.length}</ColumnInfo>
+            </RowAttandance>
+          ))}
+        </TableAttendance>
+      </AttendanceVisible>
+    </AttendanceList>
+  );
+};
 
 export default Attandance;
--- src/components/group-page/attendance/AttendanceStyled.ts
+++ src/components/group-page/attendance/AttendanceStyled.ts
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const AttendanceList = styled.div``
-export const AttendanceHead = styled.div``
-export const BlockMenu = styled.div``
-export const AttendaneVisible = styled.div`
+export const AttendanceList = styled.div``;
+export const AttendanceHead = styled.div``;
+export const BlockMenu = styled.div``;
+export const AttendanceVisible = styled.div`
   border-radius: 5px;
   box-shadow: 0 3px 6px #272d3b36;
   margin: 0 auto;
@@ -14,23 +14,21 @@ export const AttendaneVisible = styled.div`
   padding-right: 15px;
   padding-left: 15px;
   padding-bottom: 15px;
-`
+`;
 
 export const TableAttendance = styled.table`
   text-align: center;
   margin: 0 auto;
   font-size: 16px;
   border-spacing: 0;
-`
+`;
 
 export const TableHead = styled.tr`
   color: rgb(20, 24, 34);
   border-radius: 5px;
-`
-export const ColumnHead = styled.th``
+`;
+export const ColumnHead = styled.th``;
 
-export const ColumnInfo = styled.td``
+export const ColumnInfo = styled.td``;
 
-export const RowAttandance = styled.tr``
-
-
+export const RowAttandance = styled.tr``;
