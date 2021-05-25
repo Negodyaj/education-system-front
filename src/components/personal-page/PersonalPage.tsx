592cb6523c75698a7d6e050d2fa305e857768f2b
Olga Radeeva <olgaradeev5947@mail.ru>
2021-05-25 18:41:19 +0300

    add info on page

--- src/components/personal-page/PersonalPage.tsx
+++ src/components/personal-page/PersonalPage.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dispatch } from 'redux';
 import { useDispatch, useSelector } from 'react-redux';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import './PersonalPage.css';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import { IRootState } from '../../store';
 import { sendGetRequest } from '../../services/http.service';
@@ -11,73 +11,99 @@ import { UserInput } from '../../interfaces/UserInput';
 import { currentUserUrl, usersUrl } from '../../shared/consts';
 import { User } from '../../interfaces/User';
 import { isUser } from '../../services/type-guards/user';
-
-type FormValues = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  gender: string;
-  phone: string;
-  email: string;
-  developer?: string;
-};
-
-// const for action type
-export const USERS_LIST_WRETCH_LOADED = 'USERS_LIST_WRETCH_LOADED';
-
-export type TagsPageActions = ReturnType<typeof setUsersListWasLoaded>;
-
-// action creator
-export const getUser = (userId?: string) => (dispatch: Dispatch) => {
-  sendGetRequest<User>(`${usersUrl}/${userId}`, isUser).then((user) =>
-    dispatch(setUsersListWasLoaded(user))
-  );
-};
-
-// action
-export const setUsersListWasLoaded = (user: User[]) =>
-  ({
-    type: USERS_LIST_WRETCH_LOADED,
-    payload: user,
-  } as const);
+import FormElement from '../../shared/components/form-elements/FormElement';
+import { getUserToEdit, sendUser } from '../../store/user-page/action-creators';
+import { getUserFormElementSettings } from '../../shared/helpers/userFormRegisterSettingByKey';
+import { InputNames } from '../../enums/inputNames';
+import { getEnToRuTranslation } from '../../shared/converters/enumToDictionaryEntity';
+import { Role } from '../../enums/role';
 
 const PersonalPage = () => {
-  const [changeForm, setChangeForm] = useState(true);
-  const pageState = useSelector((state: IRootState) => state.userPage);
   const dispatch = useDispatch();
+  const appState = useSelector((state: IRootState) => state);
+  const user = appState.userPage.userForUserPage;
   const { idToEdit } = useParams<{ idToEdit?: string }>();
-
+  const history = useHistory();
   useEffect(() => {
-    // dispatch(getUserToEditById('1'))
-    dispatch(getUser('1'));
+    dispatch(getUserToEdit('1'));
   }, []);
+  const { ...methods } = useForm<UserInput>();
+  useEffect(() => {
+    Object.keys(appState.userPage.userForUserPage).map((key) => {
+      methods.setValue(
+        key as keyof UserInput,
+        appState.userPage.userForUserPage[key as keyof UserInput]
+      );
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
-  const onSubmit = (data: FormValues) => {
-    console.log(data);
+      return undefined;
+    });
+  }, [appState.userPage.userForUserPage]);
+  const onSubmit = (data: UserInput) => {
+    dispatch(sendUser(data, appState.userPage.userForUserPageId, history));
   };
-
-  const changeEvent = () => {
-    changeForm ? setChangeForm(false) : setChangeForm(true);
+  const closeUserPage = () => {
+    history.push('/user-list');
   };
 
   return (
     <div>
-      <button
-        className={changeForm ? 'show common-button' : 'notshow common-button'}
-        onClick={changeEvent}>
-        {console.log(pageState.userForUserPage.firstName)}
-        change
-      </button>
-      <button
-        className={changeForm ? 'notshow common-button' : 'show common-button'}
-        onClick={changeEvent}>
-        сохранить
-      </button>
+      <div className="show-user">
+        <img
+          src={appState.userPage.userForUserPage.userPic}
+          className="user-avatar"
+          alt="your avatar"
+        />
+        <div className="user-info">
+          <div className="user-name">
+            <p>{appState.userPage.userForUserPage.lastName}</p>
+            <p>{appState.userPage.userForUserPage.firstName}</p>
+          </div>
+          <div className="roles">
+            {appState.userPage.userForUserPage.roles?.map((key) => (
+              <p>{getEnToRuTranslation(Role[key])}</p>
+            ))}
+          </div>
+          <div className="birth-date">
+            Дата рождения {appState.userPage.userForUserPage.birthDate}
+          </div>
+          <ul className="contact-info">
+            Контактная информация
+            <li>{appState.userPage.userForUserPage.login}</li>
+            <li>{appState.userPage.userForUserPage.email}</li>
+            <li>{appState.userPage.userForUserPage.phone}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="edit-user">
+        <FormProvider {...methods}>
+          <div className="user-edit-form needs-validation was-validated">
+            <form className="personal-page-form">
+              {Object.keys(appState.userPage.userForUserPage).map((key) => (
+                <FormElement
+                  formElementSettings={getUserFormElementSettings(
+                    key as InputNames
+                  )}
+                  key={key}
+                />
+              ))}
+              <div className="form-row form-row-button">
+                <button
+                  className="common-button"
+                  type="button"
+                  onClick={closeUserPage}>
+                  отмена
+                </button>
+                <button
+                  className="common-button"
+                  type="button"
+                  onClick={methods.handleSubmit(onSubmit)}>
+                  сохранить
+                </button>
+              </div>
+            </form>
+          </div>
+        </FormProvider>
+      </div>
     </div>
   );
 };
